67d4fa1b146720f1105791ba6e94e309ff8b8600
Troy Christian <apostasy8@gmail.com>
2019-11-14 18:23:39 -0500

    Part 3 step 1 complete

---
+++ Photos/Photos/PhotosCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Photos
+//
+//  Created by Lambda_School_Loaner_219 on 11/14/19.
+//  Copyright © 2019 Lambda_School_Loaner_219. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageViewOutlet: UIImageView!
+    
+    @IBOutlet weak var labelOutlet: UILabel!
+}
